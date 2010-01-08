e1c2a0f0288d96726139ceeb7dc2a6a57ff0f05e
Jonathan Aquino <jon@ning.com>
2010-01-08 01:10:59 -0800

    Removed hardcoded references to jonathanaquino.com.
    Why: To make the code more portable.

--- app/views/table/list.php
+++ app/views/table/list.php
@@ -4,15 +4,15 @@
 you know that you need to memorize six tables in the back of <cite>Wheelock’s Latin</cite>.
 This website will help you to memorize those tables (and you won’t need to buy a copy of Wheelock).
 Instead of writing the tables out by hand, you can simply type them into this website and we’ll tell you
-which words are correct/incorrect. <a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>&demo=1">Check out the demo.</a>
+which words are correct/incorrect. <a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>&demo=1">Check out the demo.</a>
 </p>
 <p>Step 1: Choose a table:</p>
 <ol>
-    <li><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>">Nouns</a></li>
-    <li><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::ADJECTIVES) ?>">Adjectives</a></li>
-    <li><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_INDICATIVE_ACTIVE) ?>">Verbs: Indicative Active</a></li>
-    <li><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_INDICATIVE_PASSIVE) ?>">Verbs: Indicative Passive</a></li>
-    <li><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_SUBJUNCTIVE_ACTIVE) ?>">Verbs: Subjunctive Active</a></li>
-    <li><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_SUBJUNCTIVE_PASSIVE) ?>">Verbs: Subjunctive Passive</a></li>
+    <li><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>">Nouns</a></li>
+    <li><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::ADJECTIVES) ?>">Adjectives</a></li>
+    <li><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_INDICATIVE_ACTIVE) ?>">Verbs: Indicative Active</a></li>
+    <li><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_INDICATIVE_PASSIVE) ?>">Verbs: Indicative Passive</a></li>
+    <li><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_SUBJUNCTIVE_ACTIVE) ?>">Verbs: Subjunctive Active</a></li>
+    <li><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::VERBS_SUBJUNCTIVE_PASSIVE) ?>">Verbs: Subjunctive Passive</a></li>
 </ol>
 <?php require_once './app/views/shared/_footer.php' ?>
--- app/views/table/show.php
+++ app/views/table/show.php
@@ -1,7 +1,7 @@
 <?php require_once './app/views/shared/_header.php' ?>
 <h1><?php echo qh($this->table->getName()) ?></h1>
 <p>Here are the answers for the <cite><?php echo qh($this->table->getName()) ?></cite> table.
-<a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode($this->table->getName()) ?>">Ready to try it?</a></p>
+<a href="index.php?controller=table&action=new&table=<?php echo urlencode($this->table->getName()) ?>">Ready to try it?</a></p>
 <pre>
 <?php echo qh($this->table->getText()) ?>
 </pre>

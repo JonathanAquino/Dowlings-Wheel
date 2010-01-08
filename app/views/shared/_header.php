a47452ca507eb59112333eb622fb9aa3bb033dcc
Jonathan Aquino <jon@ning.com>
2010-01-08 00:43:06 -0800

    Removed hardcoded references to jonathanaquino.com.
    Why: To improve portability.

--- app/views/shared/_header.php
+++ app/views/shared/_header.php
@@ -3,23 +3,23 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <title>Dowling’s Wheel: Tables for Learning Latin by the Dowling Method</title>
-<link href="http://jonathanaquino.com/latin/public/stylesheets/styles.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="public/stylesheets/styles.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
 <div id="wrapper">
 <div id="header">
 	<div id="logo">
-		<h1><a href="http://jonathanaquino.com/latin/index.php">Dowling’s Wheel</a></h1>
+		<h1><a href="index.php">Dowling’s Wheel</a></h1>
 		<p>Tables for Learning Latin by the Dowling Method</p>
 	</div>
 </div>
 <div id="menu">
 	<ul>
-        <li <?php if (ifsetor($this->page) === 'home') { echo 'class="current_page_item"'; } ?>><a href="http://jonathanaquino.com/latin/index.php">Home</a></li>
-		<li <?php if (ifsetor($this->page) === 'demo') { echo 'class="current_page_item"'; } ?>><a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>&demo=1">Demo</a></li>
+        <li <?php if (ifsetor($this->page) === 'home') { echo 'class="current_page_item"'; } ?>><a href="index.php">Home</a></li>
+		<li <?php if (ifsetor($this->page) === 'demo') { echo 'class="current_page_item"'; } ?>><a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>&demo=1">Demo</a></li>
 		<li <?php if (ifsetor($this->page) === 'blog') { echo 'class="current_page_item"'; } ?>><a href="http://jonaquino.blogspot.com/2009/12/dowlings-wheel-webapp-i-wrote-to-assist.html">Blog</a></li>
-		<li <?php if (ifsetor($this->page) === 'about') { echo 'class="current_page_item"'; } ?>><a href="http://jonathanaquino.com/latin/index.php?controller=help&action=show&id=about">About</a></li>
-		<li <?php if (ifsetor($this->page) === 'contact') { echo 'class="current_page_item"'; } ?>><a href="http://jonathanaquino.com/latin/index.php?controller=help&action=show&id=contact">Contact</a></li>
+		<li <?php if (ifsetor($this->page) === 'about') { echo 'class="current_page_item"'; } ?>><a href="index.php?controller=help&action=show&id=about">About</a></li>
+		<li <?php if (ifsetor($this->page) === 'contact') { echo 'class="current_page_item"'; } ?>><a href="index.php?controller=help&action=show&id=contact">Contact</a></li>
 	</ul>
 </div>
 <div id="page">
--- app/views/table/new.php
+++ app/views/table/new.php
@@ -8,14 +8,14 @@ if ($this->demoing) { ?>
 <h1><?php echo qh($this->table->getName()) ?></h1>
 <p>Fill in the table as best you can, then press Submit. We’ll show you which
 values are right and which are wrong. If needed,
-<a href="http://jonathanaquino.com/latin/index.php?controller=table&action=show&table=<?php echo urlencode($this->table->getName()) ?>">take a peek at the answers</a>.
+<a href="index.php?controller=table&action=show&table=<?php echo urlencode($this->table->getName()) ?>">take a peek at the answers</a>.
 <?php
 if (! $this->demoing) { ?>
-    Or check out the <a href="http://jonathanaquino.com/latin/index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>&demo=1">demo</a>.
+    Or check out the <a href="index.php?controller=table&action=new&table=<?php echo urlencode(TableLoader::NOUNS) ?>&demo=1">demo</a>.
 <?php
 } ?>
 </p>
-<form action="http://jonathanaquino.com/latin/index.php?controller=table&action=create&table=<?php echo urlencode($this->table->getName()) ?>" method="post">
+<form action="index.php?controller=table&action=create&table=<?php echo urlencode($this->table->getName()) ?>" method="post">
 <input type="submit" class="submit" value="Submit" />
 <textarea id="text" name="text"><?php echo qh($this->text) ?></textarea>
 <input type="submit" class="submit" value="Submit" />
@@ -24,8 +24,8 @@ if (! $this->demoing) { ?>
 if (! $this->demoing) { ?>
     <textarea id="answers" style="display:none"><?php echo qh($this->answers) ?></textarea>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-    <script src="http://jonathanaquino.com/latin/public/javascripts/shared/util.js"></script>
-    <script src="http://jonathanaquino.com/latin/public/javascripts/table/new.js"></script>
+    <script src="public/javascripts/shared/util.js"></script>
+    <script src="public/javascripts/table/new.js"></script>
 <?php
 } ?>
 <?php require_once './app/views/shared/_footer.php' ?>

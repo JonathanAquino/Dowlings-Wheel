684da0d84a531f77bbc6f13089d8e5710e5381a9
Jon Aquino <jona@ning.com>
2016-11-10 23:38:22 -0800

    Add missing pdf file, and add link to it.

--- app/views/help/goodies.php
+++ app/views/help/goodies.php
@@ -3,5 +3,6 @@
 <p><a href="public/DowlingsWheel.pdf"><img class="thumbnail" src="public/images/pdf-thumbnail.png"/></a></p>
 <p>Print out these tables and take them on the bus, on the train, on your daily commute.
 Memorize Latin declensions while waiting in line at the grocery store:
+<a href="public/DowlingsWheel.pdf">pdf</a>
 <a href="/latin/db/data.txt">txt</a>.</p>
 <?php require_once './app/views/shared/_footer.php' ?>
